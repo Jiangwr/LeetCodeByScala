376af3b1ddc393d3d7c39ca2ab4ceb4a124f01c5
jiangwr <115207766@qq.com>
2019-07-09 17:27:26 +0800

    leetcode136 for scala

--- src/main/scala/leetCode/LeetCode136.scala
+++ src/main/scala/leetCode/LeetCode136.scala
@@ -4,6 +4,14 @@ class LeetCode136 {
 
   //Time:O(n) Space:O(1)
   def singleNumber(nums: Array[Int]): Int = {
+    var res = 0
+    for (num <- nums) res = res ^ num
+    res
+  }
+
+  //Time:O(n) Space:O(1)
+  def singleNumber2(nums: Array[Int]): Int = {
     nums.foldLeft(0)((res, num) => res ^ num)
   }
+
 }
